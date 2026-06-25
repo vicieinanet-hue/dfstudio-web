89a74df1f2f586da14e4efe8fe3f63973400d4dd
vicieinanet-hue <vicieinanet@gmail.com>
2026-06-25 09:18:52 -0300

    Add files via upload

---
+++ dfstudio-web/sw.js
@@ -0,0 +1,5 @@
+var CACHE='dfstudio-v4';
+var ASSETS=['/','/index.html','/app.js','/manifest.json'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}));self.skipWaiting()});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==CACHE}).map(function(x){return caches.delete(x)}))}));self.clients.claim()});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(c){return c||fetch(e.request)}))});
