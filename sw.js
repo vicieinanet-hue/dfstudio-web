5bb3e0a1ed84b8bca3f594a253a49fd16f7526ff
vicieinanet-hue <vicieinanet@gmail.com>
2026-06-25 11:22:37 -0300

    Update service worker to version 4 and add assets

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-var CACHE='dfstudio-v6';var ASSETS=['/','/index.html','/manifest.json'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}));self.skipWaiting()});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==CACHE}).map(function(x){return caches.delete(x)}))}));self.clients.claim()});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(c){return c||fetch(e.request)}))});
+const CACHE = 'dfstudio-v4';
+const ASSETS = ['/', '/index.html', '/app.js', '/audio-processor.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => {
+        return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    }));
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
+});
