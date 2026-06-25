9e279772ab6a3dd67e694c6089808d4d552656a5
vicieinanet-hue <vicieinanet@gmail.com>
2026-06-25 09:13:20 -0300

    Update service worker cache version and assets

--- dfstudio-web/sw.js
+++ dfstudio-web/sw.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = 'dfstudio-v2';
-var ASSETS = ['/','/index.html','/css/style.css','/js/app.js','/js/audio-engine.js','/manifest.json'];
-self.addEventListener('install',function(e){ e.waitUntil(caches.open(CACHE_NAME).then(function(cache){return cache.addAll(ASSETS);})); self.skipWaiting(); });
-self.addEventListener('activate',function(e){ e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(key){return key!==CACHE_NAME;}).map(function(key){return caches.delete(key);}));})); self.clients.claim(); });
-self.addEventListener('fetch',function(e){ e.respondWith(caches.match(e.request).then(function(cached){return cached||fetch(e.request);})); });
+var CACHE='dfstudio-v3';
+var ASSETS=['/','/index.html','/app.js','/manifest.json'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}));self.skipWaiting()});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==CACHE}).map(function(x){return caches.delete(x)}))}));self.clients.claim()});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(c){return c||fetch(e.request)}))});
