f1a3d0b3f2677468c5292724e92700d3973dafc6
vicieinanet-hue <vicieinanet@gmail.com>
2026-06-25 08:29:21 -0300

    Update service worker to version 2 and assets

--- dfstudio-web/sw.js
+++ dfstudio-web/sw.js
@@ -1,16 +1,5 @@
-const CACHE_NAME = 'dfstudio-v1';
-const ASSETS = ['/','/index.html','/css/style.css','/js/app.js','/js/audio-engine.js','/js/effects.js','/js/markers.js','/js/ui.js','/icons/icon-192.png','/icons/icon-512.png'];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
-});
+var CACHE_NAME = 'dfstudio-v2';
+var ASSETS = ['/','/index.html','/css/style.css','/js/app.js','/js/audio-engine.js','/manifest.json'];
+self.addEventListener('install',function(e){ e.waitUntil(caches.open(CACHE_NAME).then(function(cache){return cache.addAll(ASSETS);})); self.skipWaiting(); });
+self.addEventListener('activate',function(e){ e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(key){return key!==CACHE_NAME;}).map(function(key){return caches.delete(key);}));})); self.clients.claim(); });
+self.addEventListener('fetch',function(e){ e.respondWith(caches.match(e.request).then(function(cached){return cached||fetch(e.request);})); });
